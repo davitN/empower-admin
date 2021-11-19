4aa579ef11afd004e0ffb3f6ea11e5aba27bd845
Your Name <davit@davits-MacBook-Pro.local>
2021-11-19 13:03:52 +0400

    first commit

---
+++ src/components/shared/Header.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { createUseStyles } from "react-jss";
+import { Link } from "react-router-dom";
+const useStyles = createUseStyles({
+  container: {
+    width: "100%",
+    height: "80px",
+    backgroundColor: "red",
+  },
+});
+function Header() {
+  const classes = useStyles();
+  return (
+    <div className={classes.container}>
+      <Link to="/companies">companies</Link> | <Link to="/app-users">app users</Link> | <Link to="/auth">log in</Link>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/routes/AppUsers.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AppUsers() {
+  return <div>App Users</div>;
+}
+
+export default AppUsers;
