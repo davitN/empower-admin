443008df99d25b3559396305154a90316f6a088d
LGabelashvili <gabelashvili1999@gmail.com>
2021-11-24 22:03:56 +0400

    add prettire config

--- src/components/Payments/index.tsx
+++ src/components/Payments/index.tsx
@@ -17,38 +17,27 @@ const useStyles = createUseStyles({
   },
   container: {
     maxWidth: '70rem',
-    width: '100%'
+    width: '100%',
   },
 });
 
 const Payments = () => {
   const classes = useStyles();
 
-
-
   return (
     <div className="p-d-flex p-flex-column p-jc-center p-ai-center">
       <div className={classes.logo}>Logo</div>
-      <div className={classNames(
-          'p-d-flex p-flex-column p-jc-center',
-          classes.title
-        )}
-      >
-
+      <div className={classNames('p-d-flex p-flex-column p-jc-center', classes.title)}>
         <h1>JOIN THE EMPOWER PROGRAM</h1>
         <p className="p-pt-4 p-pb-6">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus nam,
-          tempore ex libero doloribus aliquid illo blanditiis ea architecto hic,
-          consectetur in eius enim labore quis id. Similique unde voluptatem
-          tempore quidem quo repellendus odio magni expedita sit, libero, optio
-          nemo corrupti consequuntur ullam accusantium? Aliquid blanditiis
-          aliquam earum quasi corrupti vitae rem voluptatum debitis adipisci.
-          Natus delectus qui illo!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus nam, tempore ex libero doloribus aliquid
+          illo blanditiis ea architecto hic, consectetur in eius enim labore quis id. Similique unde voluptatem tempore
+          quidem quo repellendus odio magni expedita sit, libero, optio nemo corrupti consequuntur ullam accusantium?
+          Aliquid blanditiis aliquam earum quasi corrupti vitae rem voluptatum debitis adipisci. Natus delectus qui
+          illo!
         </p>
       </div>
-      <div className={classNames(classes.container, 'p-shadow-3 p-d-flex')}>
-        qwdqwd
-      </div>
+      <div className={classNames(classes.container, 'p-shadow-3 p-d-flex')}>qwdqwd</div>
     </div>
   );
 };
