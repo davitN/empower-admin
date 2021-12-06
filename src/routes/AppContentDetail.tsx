5b76e67fb43e46118067c67668b515eced332428
LGabelashvili <gabelashvili1999@gmail.com>
2021-12-06 20:31:18 +0400

    implement ui

--- src/routes/AppContentDetail.tsx
+++ src/routes/AppContentDetail.tsx
@@ -1,5 +1,72 @@
+import { createUseStyles } from 'react-jss';
+import Container from '../components/shared/Container';
+import Title from '../components/shared/Title';
+import Label from '../components/shared/Inputs/Label';
+import RadioButtonComponent from '../components/shared/Inputs/RadioButton';
+
+const contentType = {
+  monthly: 'Monthly Team Activity',
+  community: 'Community Article',
+};
+
+const teamActivity = {
+  kickOff: 'KickOff',
+  ethos: 'Ethos',
+  powerUp: 'Power up',
+  gratitude: 'Gratitude',
+  powerDown: 'Power down',
+};
+
 const AppContentDetail = () => {
-  return <p>app content detail</p>;
+  const classes = useStyles();
+  return (
+    <Container sectionTitle="EDIT CONTENT">
+      <div className={classes.root}>
+        <div>
+          <Title title="Edit Content Details" fontSize="text-2xl" costumeStyles="p-mb-3" />
+          <div className="p-d-flex p-flex-column">
+            <Label label="Payment" costumeStyles="p-mb-2" />
+            <div className="p-d-flex">
+              <RadioButtonComponent
+                label={contentType.monthly}
+                checked
+                onChange={() => undefined}
+                costumeClasses="p-mr-3"
+              />
+              <RadioButtonComponent
+                label={contentType.community}
+                checked={false}
+                onChange={() => undefined}
+              />
+            </div>
+          </div>
+        </div>
+        <div>
+          <Title title="Monthly Team Activity Details" fontSize="text-2xl" costumeStyles="p-mb-3" />
+          <div className="p-d-flex p-flex-column">
+            <Label label="Monthly Team Activity" costumeStyles="p-mb-2" />
+            <div className="p-d-flex">
+              {Object.values(teamActivity).map((val) => (
+                <RadioButtonComponent
+                  label={val}
+                  checked
+                  onChange={() => undefined}
+                  costumeClasses="p-mr-3"
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default AppContentDetail;
+
+const useStyles = createUseStyles({
+  root: {
+    display: 'grid',
+    gridRowGap: '3rem',
+  },
+});
