32448a231896f45cd5a2e0c795d214dfeb212c69
LGabelashvili <gabelashvili1999@gmail.com>
2021-12-19 22:31:14 +0400

    fx typo

--- src/components/LogIn/LogIn.tsx
+++ src/components/LogIn/LogIn.tsx
@@ -30,7 +30,7 @@ const Auth = () => {
     <div className={classes.root}>
       <h1 className="text-3xl p-flex-initial flex-column p-mb-2">LOG IN</h1>
       <p className={classNames('text-sm p-mb-4 p-mt-0', classes.forgotPassword)}>
-        Forget your pasword? Click
+        Forget your password? Click
         <Link to="/">here.</Link>
       </p>
       <div className="p-d-flex p-flex-column">
