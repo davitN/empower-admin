6ee042bc970d92a5bb96819c3645f7703e67850f
LGabelashvili <gabelashvili1999@gmail.com>
2021-12-25 21:47:56 +0400

    Add Analytics boxes

--- src/routes/Analitycs.tsx
+++ src/routes/Analitycs.tsx
@@ -2,6 +2,8 @@ import classNames from 'classnames';
 import { createUseStyles } from 'react-jss';
 import Container from '../components/shared/Container';
 import COLORS from '../services/colors.service';
+import Title from '../components/shared/Title';
+import AnalyticBox from '../components/shared/AnalyticsBox';
 
 const Analytics = () => {
   const classes = useStyles();
@@ -13,6 +15,10 @@ const Analytics = () => {
         BEGINS WITH
         <span>YOU</span>
       </h1>
+      <Title title="ANALYTICS" fontSize="text-2xl" costumeStyles="p-mt-6" />
+      <div className={classes.boxes}>
+        {new Array(14).fill(1).map(() => <AnalyticBox title="Daily Check-Ins Completed" value="2,500" desc="The total number of users created through the admin panel" />)}
+      </div>
     </Container>
   );
 };
@@ -28,5 +34,10 @@ const useStyles = createUseStyles({
       color: COLORS.orange,
       padding: '0 10px',
     },
+  },
+  boxes: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fill, minmax(18rem,1fr))',
+    gridGap: '3rem',
   },
 });
