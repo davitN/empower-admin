312b9e7ae1a4338be63d44ec33579928c9ed26eb
LGabelashvili <gabelashvili1999@gmail.com>
2021-12-25 22:03:07 +0400

    implement analytics page

---
+++ src/components/shared/ProgressBar.tsx
@@ -0,0 +1,37 @@
+import { ProgressBar } from 'primereact/progressbar';
+import { createUseStyles } from 'react-jss';
+import COLORS from '../../services/colors.service';
+
+const ProgressBarComponent = ({
+  firstTitle, secondTitle, value, customStyles,
+}: { firstTitle: string, secondTitle:string, value: string, customStyles?: any }) => {
+  const classes = useStyles();
+  return (
+    <div className={customStyles}>
+      <p className={classes.title}>
+        {firstTitle}
+        <span>{secondTitle}</span>
+      </p>
+      <ProgressBar value={value} showValue={false} className={classes.root} />
+    </div>
+  );
+};
+
+export default ProgressBarComponent;
+
+const useStyles = createUseStyles({
+  root: {
+    height: '0.8rem',
+    '& > div': {
+      background: `${COLORS.orange} !important`,
+    },
+  },
+  title: {
+    color: COLORS.blueWood,
+    fontWeight: 600,
+    marginBottom: '0.5rem',
+    '& > span': {
+      color: COLORS.orange,
+    },
+  },
+});
--- src/routes/Analitycs.tsx
+++ src/routes/Analitycs.tsx
@@ -4,6 +4,7 @@ import Container from '../components/shared/Container';
 import COLORS from '../services/colors.service';
 import Title from '../components/shared/Title';
 import AnalyticBox from '../components/shared/AnalyticsBox';
+import ProgressBar from '../components/shared/ProgressBar';
 
 const Analytics = () => {
   const classes = useStyles();
@@ -16,8 +17,18 @@ const Analytics = () => {
         <span>YOU</span>
       </h1>
       <Title title="ANALYTICS" fontSize="text-2xl" costumeStyles="p-mt-6" />
-      <div className={classes.boxes}>
+      <div className={classNames(classes.boxes, 'p-pt-5 p-pb-6')}>
         {new Array(14).fill(1).map(() => <AnalyticBox title="Daily Check-Ins Completed" value="2,500" desc="The total number of users created through the admin panel" />)}
+      </div>
+      <div className={classes.wrapper}>
+        <div>
+          <Title title="Top 7 dimensions" costumeStyles="p-mb-5" />
+          {new Array(8).fill(1).map(() => <ProgressBar firstTitle="PHYSICAL - " secondTitle="ACCOUNTABILITY - 12" value={(25 * Math.random()).toString()} customStyles="p-mb-3" />)}
+        </div>
+        <div>
+          <Title title="Top Ethos cards" costumeStyles="p-mb-5" />
+          {new Array(8).fill(1).map(() => <ProgressBar firstTitle="PHYSICAL - " secondTitle="ACCOUNTABILITY - 12" value={(25 * Math.random()).toString()} customStyles="p-mb-3" />)}
+        </div>
       </div>
     </Container>
   );
@@ -39,5 +50,10 @@ const useStyles = createUseStyles({
     display: 'grid',
     gridTemplateColumns: 'repeat(auto-fill, minmax(18rem,1fr))',
     gridGap: '3rem',
+  },
+  wrapper: {
+    display: 'grid',
+    gridAutoFlow: 'column',
+    gridGap: '5rem',
   },
 });
