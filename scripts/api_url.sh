874136f2cae00cf9c4a3464447a79137d3f2a000
George Qoridze <george@qoridze.com>
2021-12-29 13:46:23 +0400

    admin pipeline, fix

--- scripts/api_url.sh
+++ scripts/api_url.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 if [ "$BUILD_ENV" = "prod"  ];
-then sed -Ei "s/(export const backendUrl =).*/\1 'https\:\/\/api.empowerofyou.com\/admin\/';/g"  src/services/credentials.service.ts
+then 
+  echo "Production build, replacing API URL..."
+  sed -Ei "s/(export const backendUrl =).*/\1 'https\:\/\/api.empowerofyou.com\/admin\/';/g"  src/services/credentials.service.ts
+else
+  echo "Skiping API replacement..."
 fi
